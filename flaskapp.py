d1dabbd3165d93726d918607a93192db4087b7c4
Memhir-Yasue <eyasu.woldu@temple.edu>
2018-05-14 08:29:53 -0400

    Currently working with implementing functions from redditnlp.py

--- flaskapp.py
+++ flaskapp.py
@@ -2,14 +2,13 @@ from flask import Flask, request, render_template, session
 import redditnlp
 
 
-
+# redditnlp.version110()
 
 
 
 app = Flask(__name__)
 @app.route('/')
 def index():
-	# redditnlp.version106()
-	return 'Hello, World'
+	return redditnlp.version111_flask()
 if __name__ == "__main__":
 	app.run(debug=True)
