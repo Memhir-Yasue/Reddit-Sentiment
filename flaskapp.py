25b8c9a017c3ee5479f7a1a3262e26da7f13fdc3
Memhir-Yasue <eyasu.woldu@temple.edu>
2018-05-15 03:43:03 -0400

    Able to display the results of the function from redditnlp. Still needs work to display everything as a line

--- flaskapp.py
+++ flaskapp.py
@@ -7,8 +7,18 @@ import redditnlp
 
 
 app = Flask(__name__)
+
 @app.route('/')
 def index():
-	return redditnlp.version111_flask()
+	ni = []
+
+@app.route('/newpage')
+def newpage():
+	num_array = [redditnlp.version120_flask() for i in range(10)]
+	return render_template("index.html", num_array=num_array)
+
+
+
+
 if __name__ == "__main__":
 	app.run(debug=True)
