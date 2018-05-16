1045ff030a51e7be36855b42528266bf3af4bb41
Memhir-Yasue <eyasu.woldu@temple.edu>
2018-05-16 12:08:33 -0400

    Temp fix to make dash app run in flask

--- flaskapp.py
+++ flaskapp.py
@@ -1,25 +1,31 @@
+import flask
 from flask import Flask, request, render_template, session
+import dash
+import dash_html_components as html
 import redditnlp
-
-
 # redditnlp.version110()
 
 
 
-app = Flask(__name__)
 
-@app.route('/')
+server = flask.Flask(__name__)
+
+
+@server.route('/')
 def index():
-	ni = []
+	return 'Welcome'
 
-@app.route('/newpage')
-def newpage():
+@server.route('/result')
+def result():
 	main_info = redditnlp.version125_flask(10)
-	# main_infoee = ['Hi','I like','YOU!','bye!']
 	return render_template("index.html", main_info=main_info)
 
+# dash app
+app = dash.Dash(__name__, server=server, url_base_pathname='/dashapp')
+app.layout = html.Div(children=[
+    html.H1(children='Dash App')])
 
 
 
 if __name__ == "__main__":
-	app.run(debug=True)
+	server.run(debug=True)
