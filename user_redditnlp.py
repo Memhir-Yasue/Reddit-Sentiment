b479e04c0b9980baf8b47ebfbc73e3ba84f0dfc2
Memhir-Yasue <eyasu.woldu@temple.edu>
2018-05-31 16:04:18 -0400

    occurance under a subreddit added

--- user_redditnlp.py
+++ user_redditnlp.py
@@ -14,27 +14,57 @@ def user_frm_subreddit():
 		for comment in comments:
 			i+=1
 			user.append(comment.author)
-			print(i, comment.author)
 	return user
-
 user = user_frm_subreddit()
-print(user)
 
+
+
+
+
+
+#
+#	Part 1.5
+#
+def repetition(user):
+	occurance_list = []
+	name_list = []
+	for i in range(len(user)):
+		occurance = user.count(user[i])
+		name = user[i]
+		name_list.append(name)
+		occurance_list.append(occurance)
+
+# repetition(user)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#
+#				FOR PART 2
+#
 def comments_frm_user(user): # return an array with each element of the array being all the comments from each redditor
 	print(len(user))
 	all_comment = []
+	comments_frm_redditors = all_comment
 	for i in range(len(user)): # For all users 
 		redditer = user[i]
 		redditer_comment = []
 		for comment in reddit.redditor(str(redditer)).comments.new(limit=50):	#Get 50 comments from each redditor
-			# print(user[item],comment.body,'\n')
 			redditer_comment.append(comment.body)
-		all_comment.append(redditer_comment)			# store each collection of comments (from each redditor) into a list
-		print(i,redditer, all_comment[i])
+		comments_frm_redditors.append(redditer_comment)			# store each collection of comments (from each redditor) into a list
+		print(i,redditer, comments_frm_redditors)
 		print('\n')
 
-	# for i in range(len(all_comment)):
-	# 	print(i,all_comment[i],i)
-
-
-comments_frm_user(user)
+# comments_frm_user(user)
